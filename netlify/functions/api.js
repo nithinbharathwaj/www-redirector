45cc370df6582c5e24e95c0b9938440744fcedaf
Nithin Bharadwaj <nithin.bharadwaj@vonage.com>
2024-04-19 18:32:26 +0530

    Fix incorrect URL formation in redirect
    The previous implementation prefixed an unnecessary "protocol" before the wwwDomain while redirecting. This has been fixed by directly using the wwwDomain in the redirection step. This change makes the URL formation more accurate.

--- netlify/functions/api.js
+++ netlify/functions/api.js
@@ -4,7 +4,7 @@ import serverless from "serverless-http";
 
 app.get('*', (req, res) => {
     const wwwDomain = `https://www.${req.headers.host}${req.url}`;
-    return res.redirect(301, `${protocol}://${wwwDomain}`);
+    return res.redirect(301, `${wwwDomain}`);
 });
 
 export const handler = serverless(app);
