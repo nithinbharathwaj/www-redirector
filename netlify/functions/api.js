ccbeb7bfea7ea1837a1342dbab80d33d14ce5c6f
Nithin Bharadwaj <nithin.bharadwaj@vonage.com>
2024-04-19 18:29:04 +0530

    Migrate API to Netlify serverless function
    In this commit, the API is migrated to use Netlify serverless functions, moving from the `api/index.js` to `netlify/functions/api.js`. The port listener is replaced with Serverless-HTTP handlers, dependencies for serverless-HTTP and Netlify functions are added,

--- netlify/functions/api.js
+++ netlify/functions/api.js
@@ -1,12 +1,10 @@
 const express = require('express');
 const app = express();
-const port = 3000; // or any port you prefer
+import serverless from "serverless-http";
 
 app.get('*', (req, res) => {
     const wwwDomain = `https://www.${req.headers.host}${req.url}`;
     return res.redirect(301, `${protocol}://${wwwDomain}`);
 });
 
-app.listen(port, () => {
-    console.log(`Server running on port ${port}`);
-});
+export const handler = serverless(api);
